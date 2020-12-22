266ffbe907fd67f11e05c7b609dfffa47de20931
Peter Akaliro <pin4eva@hotmail.com>
2020-12-22 16:17:26 +0100

    update 3

--- src/components/ApointmentModalComp.tsx
+++ src/components/ApointmentModalComp.tsx
@@ -9,7 +9,7 @@ const ApointmentModalComp = () => {
             <div
               className="modal fade order-model"
               id="exampleModal0"
-              tabIndex="-1"
+              tabIndex={-1}
               role="dialog"
               aria-hidden="true"
             >
